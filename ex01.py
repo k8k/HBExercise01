c0117704171632fc95ff5c90164ea43f9bd5c3b2
Hackbright Student <info@hackbrightacademy.com>
2014-09-30 16:25:40 -0700

    fixed ability to check if it is an integer, converted it to an integer, and then allowed player to play again with new guess, and not asking for name again.  But, still broken if player does not want to play again

--- ex01.py
+++ ex01.py
@@ -1,25 +1,35 @@
 import random
+
+name = raw_input("Hi! What's your name?")
+print "Hi %s, thanks for playing our guessing game. " % name
+
 def guessGame():
-    name = raw_input("Hi! What's your name?")
-    print "Hi %s, thanks for playing our guessing game. " % name
 
     number = random.randrange(1, 101)
     count = 1
    
     while True:
         guess = raw_input("I am thinking of a number between 1 and 100. What do you think it is?")
-        if not guess.isdigit():
+        try:
+            guess = int(guess)
+        except ValueError:
             print "Please give us a real number, dummy."
-        elif guess > 100 or guess < 1:
-            print "Your number is not in the range!"
-        elif guess > number:
-            print "Your guess is too high, try again!"
-            count = count + 1
-        elif guess < number:
-            print "Your guess is too low, try again!"
-            count = count + 1
-        else:
-            print "Congratulations! You got it right! It took you", count, "times!"
-            break
+        
+        if type(guess) == int:
+            if guess > 100 or guess < 1:
+                print "Your number is not in the range!"
+            elif guess > number:
+                print "Your guess is too high, try again!"
+                count = count + 1
+            elif guess < number:
+                print "Your guess is too low, try again!"
+                count = count + 1
+            else:
+                print "Congratulations! You got it right! It took you", count, "times!"
+                playAgain = raw_input("Do you want to play again? Y or N")
+                if playAgain == "Y":
+                    guessGame()
+                else:
+                    break
 
 guessGame()
