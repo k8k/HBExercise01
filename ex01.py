c73edb1bdc150e38da9006d810627e49194e9201
Hackbright Student <info@hackbrightacademy.com>
2014-09-30 18:05:46 -0700

    final product. fixed function break problem, while still counting bad guesses (non integers and out of range). moved count_current increment to top of while loop.

--- ex01.py
+++ ex01.py
@@ -5,12 +5,13 @@ print "Hi %s, thanks for playing our guessing game. " % name    #greets user by 
 
 def guessGame(): #defines function that will run guessing game
 
-    number = random.randrange(1, 3)  #generates a random number for user to guess
+    number = random.randrange(1, 101)  #generates a random number for user to guess
     count_current = 0 #starts the count of guesses at 1
     count_best = 1000 #defines best count as 1,000 so that it will be more than count_current for 
                       #first guess
 
     while True: #starts a while loop that will run while the condition is true
+        count_current = count_current + 1
 
         guess = raw_input("I am thinking of a number between 1 and 100. What do you think it is?") 
         #asks user to pick a number and assigns that number to variable guess
@@ -18,11 +19,11 @@ def guessGame(): #defines function that will run guessing game
             guess = int(guess) #turns variable guess into an integer
         except ValueError:
             print "Please give us a real number, dummy." 
-            count_current = count_current + 1
+           
             #prints message to user if they don't enter an int
         
         if type(guess) == int: # starts if else statement if the type of the guess is an integer
-            count_current = count_current + 1
+
             if guess > 100 or guess < 1: #checks if the guess is in range between 1 and 100
                 print "Your number is not in the range!"
             elif guess > number: # if they guess too high prints that they guess too high and adds a count 
@@ -40,7 +41,7 @@ def guessGame(): #defines function that will run guessing game
                     print "You matched your high score of", count_best, ". Keep playing!"
                 else: 
                     print "The current high score is", count_best #if neither best nor tie tell previous best score
-                playAgain = raw_input("Do you want to play again? Y or N") #asks user if they want to play again
+                playAgain = raw_input("Do you want to play again? Y or N ") #asks user if they want to play again
                 if playAgain == "Y": #if they want to play again it calls the function again to resume game play
                     guessGame()
                 else:
