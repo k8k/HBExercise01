d465d7d490805abdc1e1116445f628d542d9355b
Hackbright Student <info@hackbrightacademy.com>
2014-09-30 17:59:14 -0700

    finished writing all of the comments

--- ex01.py
+++ ex01.py
@@ -6,7 +6,7 @@ print "Hi %s, thanks for playing our guessing game. " % name    #greets user by 
 def guessGame(): #defines function that will run guessing game
 
     number = random.randrange(1, 3)  #generates a random number for user to guess
-    count_current = 1 #starts the count of guesses at 1
+    count_current = 0 #starts the count of guesses at 1
     count_best = 1000 #defines best count as 1,000 so that it will be more than count_current for 
                       #first guess
 
@@ -18,33 +18,33 @@ def guessGame(): #defines function that will run guessing game
             guess = int(guess) #turns variable guess into an integer
         except ValueError:
             print "Please give us a real number, dummy." 
+            count_current = count_current + 1
             #prints message to user if they don't enter an int
         
         if type(guess) == int: # starts if else statement if the type of the guess is an integer
+            count_current = count_current + 1
             if guess > 100 or guess < 1: #checks if the guess is in range between 1 and 100
                 print "Your number is not in the range!"
-                count_current = count_current + 1
             elif guess > number: # if they guess too high prints that they guess too high and adds a count 
                                  # for the guesses
                 print "Your guess is too high, try again!"
-                count_current = count_current + 1
-            elif guess < number:
-                print "Your guess is too low, try again!"
-                count_current = count_current + 1
+            elif guess < number: #checks if guess is lower
+                print "Your guess is too low, try again!" #hint
             else:
                 print "Congratulations! You got it right! It took you", count_current, "times!"
-                if count_current < count_best:
-                    print "You have a new high score of", count_current
-                    count_current == count_best
-                elif count_current == count_best:
+                #guess was correct, tells them how many guesses
+                if count_current < count_best: #checks if guess was best score
+                    print "You have a new high score of", count_current #if so tells them
+                    count_current == count_best #redefines count_best to count_current
+                elif count_current == count_best: #checks if guess was equal to previous best score
                     print "You matched your high score of", count_best, ". Keep playing!"
                 else: 
-                    print "The current high score is", count_best 
-                playAgain = raw_input("Do you want to play again? Y or N")
-                if playAgain == "Y":
+                    print "The current high score is", count_best #if neither best nor tie tell previous best score
+                playAgain = raw_input("Do you want to play again? Y or N") #asks user if they want to play again
+                if playAgain == "Y": #if they want to play again it calls the function again to resume game play
                     guessGame()
                 else:
-                    print "Thanks for playing! We hope you come back."
-                    break
+                    print "Thanks for playing! We hope you come back." #if they don't want to play it breaks the function
+                    break                                              #then the function stops
 
-guessGame()
+guessGame()  #calls the function originally
